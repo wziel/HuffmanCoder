c85880f6cdf228915f4c020727b562a326f8f1e3
MarekM25 <marekm2504@gmail.com>
2017-03-08 23:59:21 +0100

    Add readers/writers

---
+++ HuffmanCoder.Model/Readers/IInputReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanCoder.Model.Readers
+{
+    public interface IInputReader : IEnumerator<byte[]>
+    {
+
+    }
+}
---
+++ HuffmanCoder.Model/Writers/DecoderOutputWriter.cs
@@ -0,0 +1,22 @@
+﻿using HuffmanCoder.Model.Codec;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanCoder.Model.Writers
+{
+    public class DecoderOutputWriter : IHuffmanCoderOutput
+    {
+        public DecoderOutputWriter()
+        {
+
+        }
+
+        public void Write(bool bit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
