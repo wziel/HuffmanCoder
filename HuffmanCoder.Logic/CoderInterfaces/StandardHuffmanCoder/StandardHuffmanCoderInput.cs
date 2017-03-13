bd0bf331e62b6cdcc06ad877081baf5c1c1b8e36
Michal Mudel <m.mudel@o2.pl>
2017-03-13 23:55:58 +0100

    Implement StandardHuffmanCoderInterface.

---
+++ HuffmanCoder.Logic/CoderInterfaces/StandardHuffmanCoder/StandardHuffmanCoderInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HuffmanCoder.Model.Codec;
+using HuffmanCoder.Logic.Readers.Encoding;
+
+namespace HuffmanCoder.Logic.CoderInterfaces.StandardHuffmanCoder
+{
+    class StandardHuffmanCoderInput : IHuffmanCoderInput<byte>
+    {
+
+        private IInputReader inputReader;
+
+        public StandardHuffmanCoderInput(IInputReader inputReader)
+        {
+            this.inputReader = inputReader;
+        }
+        public bool IsEnd()
+        {
+            return inputReader.MoveNext();
+        }
+
+        public byte Read()
+        {
+            byte symbol = inputReader.Current;
+            inputReader.MoveNext();
+            return symbol;
+        }
+    }
+}
---
+++ HuffmanCoder.Logic/CoderInterfaces/StandardHuffmanCoder/StandardHuffmanCoderInterface.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HuffmanCoder.Logic.Readers.Encoding;
+using HuffmanCoder.Logic.Writers.Encoding;
+using HuffmanCoder.Model.Builder;
+using HuffmanCoder.Model.Codec;
+
+namespace HuffmanCoder.Logic.CoderInterfaces.StandardHuffmanCoder
+{
+    class StandardHuffmanCoderInterface : IHuffmanCoderInterface
+    {
+        private IInputReader inputReader;
+        private ICoderOutputWriter coderOutputWriter;
+        private IComparer<byte> comparer;
+        public StandardHuffmanCoderInterface(IInputReader inputReader, ICoderOutputWriter coderOutputWriter, IComparer<byte> comparer)
+        {
+            this.inputReader = inputReader;
+            this.coderOutputWriter = coderOutputWriter;
+            this.comparer = comparer;
+        }
+        public void Encode()
+        {
+            HuffmanTreeBuilder<byte> huffmanTreeBuilder = new HuffmanTreeBuilder<byte>(comparer, createDictionary());
+            IHuffmanCoder<byte> huffmanCoder = new HuffmanCoder<byte>(huffmanTreeBuilder.BuildTree());
+            huffmanCoder.Encode(new StandardHuffmanCoderInput(inputReader), new HuffmanCoderOutput(coderOutputWriter));
+        }
+
+        private Dictionary<byte, int> createDictionary()
+        {
+            Dictionary<byte, int> symbolQuantityDic = new Dictionary<byte, int>();
+            do
+            {
+                if (symbolQuantityDic.ContainsKey(inputReader.Current))
+                {
+                    ++symbolQuantityDic[inputReader.Current];
+                }
+                else
+                {
+                    symbolQuantityDic.Add(inputReader.Current, 1);
+                }
+            } while (inputReader.MoveNext());
+
+            inputReader.Reset();
+
+            return symbolQuantityDic;
+        }
+    }
+}
