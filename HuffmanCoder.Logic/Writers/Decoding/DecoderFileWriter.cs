b0ccf2adac20d21cb4f8bf6abea401dc9024fb22
MarekM25 <marekm2504@gmail.com>
2017-03-15 18:25:41 +0100

    Implemented DecoderFileWriter

--- HuffmanCoder.Logic/Writers/Decoding/DecoderFileWriter.cs
+++ HuffmanCoder.Logic/Writers/Decoding/DecoderFileWriter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,21 +12,23 @@ namespace HuffmanCoder.Logic.Writers.Decoding
         void Save();
         void Write(byte data);
     }
-    public class DecoderFileWriter : IDecoderFileWriter
+    public sealed class DecoderFileWriter : IDecoderFileWriter
     {
+        private string filePath;
+        private List<byte> bytes = new List<byte>();
         public DecoderFileWriter(string filePath)
         {
-
+            this.filePath = filePath;
         }
 
         public void Save()
         {
-            throw new NotImplementedException();
+            File.WriteAllBytes(filePath, bytes.ToArray())
         }
 
         public void Write(byte data)
         {
-            throw new NotImplementedException();
+            bytes.Add(data);
         }
     }
 }
