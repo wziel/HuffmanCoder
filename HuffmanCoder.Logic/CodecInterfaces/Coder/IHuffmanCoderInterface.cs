ec60dcaa76569d7cd95b962f9f7b9eeef495599b
Michal Mudel <m.mudel@o2.pl>
2017-03-18 23:06:22 +0100

    Add missing file.

---
+++ HuffmanCoder.Logic/CodecInterfaces/Coder/IHuffmanCoderInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanCoder.Logic.CodecInterfaces.Coder
+{
+    public interface IHuffmanCoderInterface
+    {
+        void Encode();
+    }
+}
